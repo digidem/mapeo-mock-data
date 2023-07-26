fbb63e0897db12752140b76d9e903a2701ad192b
Andrew Chou <andrewchou@fastmail.com>
2023-07-26 12:42:47 +0100

    add ts publishing stuff

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 import path from 'node:path'
 import fs from 'node:fs'
 
-import { JSONSchemaFaker, createFakerSchema } from './faker.js'
-
-/**
- * @typedef {{version: string, path: string, schema: any}} SchemaInfo
- * @typedef {Array<SchemaInfo>} SchemaInfoList
- */
+import { JSONSchemaFaker, createFakerSchema } from './lib/faker.js'
+import {
+  readSchemas,
+  getMapeoSchemaPaths,
+  getSchemaInfo,
+} from './lib/schema.js'
 
 export function listSchemas() {
   /** @type {{[name: string]: Array<string>}} */
@@ -15,7 +15,7 @@ export function listSchemas() {
   for (const schema of getMapeoSchemaPaths().schemas) {
     // We skip `common` schema
     if (schema.name === 'common') continue
-    const schemaVersions = fs.readdirSync(schema.path)
+    const schemaVersions = fs.readdirSync(schema.filePath)
 
     result[schema.name] = schemaVersions.map((v) => path.parse(v).name)
   }
@@ -44,79 +44,11 @@ export async function generate(schemaName, { version, count } = {}) {
     result[i] = await JSONSchemaFaker.resolve(
       createFakerSchema(targetSchemaInfo.schema),
       {
-        [commonSchemaInfo.path]: createFakerSchema(commonSchemaInfo.schema),
+        [commonSchemaInfo.filePath]: createFakerSchema(commonSchemaInfo.schema),
       },
       path.join(mapeoSchemaPaths.base, schemaName),
     )
   }
 
   return result
-}
-
-/**
- * @param {Array<{name: string, path: string}>} info
- */
-function readSchemas(info) {
-  const nameToSchema = new Map()
-
-  for (const { name: schemaName, path: schemaDirectoryPath } of info) {
-    const schemaVersions = fs.readdirSync(schemaDirectoryPath)
-
-    /** @type {SchemaInfoList} */
-    const schemaInfo = []
-
-    for (let i = 0; i < schemaVersions.length; i++) {
-      const versionWithExtension = schemaVersions[i]
-      const version = path.parse(versionWithExtension).name
-      const schemaPath = path.join(schemaDirectoryPath, versionWithExtension)
-
-      schemaInfo.push({
-        version,
-        path: schemaPath,
-        schema: JSON.parse(fs.readFileSync(schemaPath, 'utf-8')),
-      })
-    }
-
-    nameToSchema.set(schemaName, schemaInfo)
-  }
-
-  return nameToSchema
-}
-
-/**
- * @param {Map<string, SchemaInfoList>} schemas
- * @param {string} name
- * @param {string} [version]
- * @returns {SchemaInfo}
- */
-function getSchemaInfo(schemas, name, version) {
-  const schemasList = schemas.get(name)
-
-  if (!schemasList) throw new Error(`Could not find schema ${name}`)
-
-  // Assumes last one is latest
-  if (!version) return schemasList[schemasList.length - 1]
-
-  const result = schemasList.find((s) => s.version === version)
-
-  if (!result) throw new Error(`Could not find version ${version}`)
-
-  return result
-}
-
-function getMapeoSchemaPaths() {
-  const basePath = new URL(
-    './node_modules/@mapeo/schema/schema',
-    import.meta.url,
-  ).pathname
-
-  const schemaDirNames = fs.readdirSync(basePath)
-
-  return {
-    base: basePath,
-    schemas: schemaDirNames.map((name) => ({
-      name,
-      path: path.join(basePath, name),
-    })),
-  }
 }
--- types.d.ts
+++
@@ -1,8 +0,0 @@
-import { JsonValue } from 'type-fest'
-
-declare function listSchemas(): { [name: string]: Array<string> }
-
-declare function generate(
-  schemaName: string,
-  opts?: { version?: string; count?: number },
-): Promise<Array<JsonValue>>
