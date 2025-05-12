1c6ab0e8dea62166125b24673511202ff121242c
Andrew Chou <andrewchou@fastmail.com>
2025-05-12 11:05:38 -0400

    separate tsconfig for publishing

--- bin/generate-mapeo-data.js
+++ bin/generate-mapeo-data.js
@@ -1,6 +1,5 @@
 #!/usr/bin/env node
 
-// @ts-check
 import fs from 'node:fs'
 import path from 'node:path'
 import { parseArgs } from 'node:util'
--- bin/list-mapeo-schemas.js
+++ bin/list-mapeo-schemas.js
@@ -1,6 +1,5 @@
 #!/usr/bin/env node
 
-// @ts-check
 import { listSchemas } from '../index.js'
 
 console.log(listSchemas())
